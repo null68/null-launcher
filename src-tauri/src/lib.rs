ec2dc356cfb83c487f991eb6e267433f7b39ae72
null68 <fariscomaga@gmail.com>
2026-08-10 01:12:13 +0200

    feat: fetch versions

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,18 @@
+mod minecraft;
+
+use crate::minecraft::manifest::{fetch_versions, get_versions};
+
+#[tauri::command]
+async fn initialize_launcher() -> Result<(), String> {
+    fetch_versions().await.map_err(|e| e.to_string())?;
+
+    Ok(())
+}
+
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        // .invoke_handler(tauri::generate_handler![])
+        .invoke_handler(tauri::generate_handler![initialize_launcher, get_versions])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src-tauri/src/minecraft/manifest.rs
+++ src-tauri/src/minecraft/manifest.rs
@@ -1,15 +1,52 @@
-#[derive(Deserialize)]
+use reqwest::{self, Client};
+use serde::{self, Deserialize, Serialize};
+
+use once_cell::sync::Lazy;
+use std::sync::Mutex;
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct VersionManifest {
     versions: Vec<Version>,
 }
 
-#[derive(Deserialize)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Version {
     id: String,
     url: String,
+    sha1: String,
+    r#type: String,
 }
 
+pub static VERSIONS: Lazy<Mutex<Option<VersionManifest>>> = Lazy::new(|| Mutex::new(None));
+
 #[tauri::command]
-pub async fn get_versions() -> Result<VersionManifest, Box<dyn std::error::Error>> {
-    // todo
+pub fn get_versions() -> Option<VersionManifest> {
+    let versions = VERSIONS.lock().unwrap();
+
+    versions.clone()
+}
+
+pub async fn fetch_versions() -> Result<VersionManifest, Box<dyn std::error::Error>> {
+    {
+        let versions = VERSIONS.lock().unwrap();
+
+        if let Some(manifest) = versions.as_ref() {
+            return Ok(manifest.clone());
+        }
+    }
+    let client = Client::new();
+    let res = client
+        .get("https://piston-meta.mojang.com/mc/game/version_manifest_v2.json")
+        .send()
+        .await?
+        .error_for_status()?;
+
+    let version_manifest: VersionManifest = res.json::<VersionManifest>().await?;
+    {
+        let mut versions = VERSIONS.lock().unwrap();
+
+        *versions = Some(version_manifest.clone());
+    }
+
+    Ok(version_manifest)
 }
