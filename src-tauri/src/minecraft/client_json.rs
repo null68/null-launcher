e724be4f5f5a0361a3399eb9ea3a22fc7dd37d71
null68 <fariscomaga@gmail.com>
2026-08-11 23:40:13 +0200

    feat: fetch <version>.json

--- src-tauri/src/minecraft/client_json.rs
+++ src-tauri/src/minecraft/client_json.rs
@@ -1,6 +1,16 @@
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    error::Error,
+    fs::{self, create_dir_all, remove_file, write},
+    path::Path,
+};
 
+use reqwest::Client;
 use serde::{self, Deserialize, Serialize};
+use serde_json::from_slice;
+use sha1::{Digest, Sha1};
+
+use crate::minecraft::manifest::Version;
 
 #[derive(Serialize, Deserialize)]
 pub struct ClientJson {
@@ -18,6 +28,16 @@ pub struct ClientJson {
 
     #[serde(rename = "minecraftArguments")]
     pub minecraft_arguments: Option<String>, // fuck you mojang
+
+    #[serde(rename = "javaVersion")]
+    pub java_version: Option<JavaVersion>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct JavaVersion {
+    pub component: String,
+    #[serde(rename = "majorVersion")]
+    pub majorVersion: u32,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -31,17 +51,27 @@ pub struct Download {
 pub struct ClientDownloads {
     pub client: Download,
     //pub server: Download,
+    pub client_mappings: Option<Download>, // if i ever get to mods in launcher
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct Library {
     pub downloads: LibraryDownloads,
     pub name: String,
+    pub rules: Option<Vec<Rule>>,
+    pub natives: Option<HashMap<String, String>>,
+    pub extract: Option<Extract>,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct LibraryDownloads {
-    pub artifact: Artifact,
+    pub artifact: Option<Artifact>,
+    pub classifiers: Option<HashMap<String, Artifact>>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Extract {
+    pub exclude: Option<Vec<String>>,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -65,8 +95,8 @@ pub struct AssetIndex {
 
 #[derive(Serialize, Deserialize)]
 pub struct Arguments {
-    pub game: Vec<Argument>,
-    pub jvm: Vec<Argument>,
+    pub game: Option<Vec<Argument>>,
+    pub jvm: Option<Vec<Argument>>,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -98,4 +128,52 @@ pub struct OS {
     pub name: Option<String>,
     pub version: Option<String>,
     pub arch: Option<String>,
+}
+
+pub async fn fetch_or_get_client_json(
+    version: &Version,
+    path_to_versions_dir: &Path,
+) -> Result<ClientJson, Box<dyn Error>> {
+    let path_to_versions_dir = path_to_versions_dir.join(&version.id);
+    let path_to_version_json = path_to_versions_dir.join(format!("{}.json", version.id));
+
+    if path_to_version_json.exists() {
+        let bytes = fs::read(&path_to_version_json)?;
+        let mut hasher = Sha1::new();
+
+        hasher.update(&bytes);
+        let hash = hasher.finalize();
+        let hash = hex::encode(hash);
+        if version.sha1 == hash {
+            let json = from_slice::<ClientJson>(&bytes)?;
+
+            return Ok(json);
+        }
+
+        remove_file(&path_to_version_json)?;
+    }
+    let client = Client::new();
+
+    let res = client
+        .get(&version.url)
+        .send()
+        .await?
+        .error_for_status()?
+        .bytes()
+        .await?;
+
+    let mut hasher = Sha1::new();
+
+    hasher.update(&res);
+    let hash = hasher.finalize();
+    let hash = hex::encode(hash);
+    if version.sha1 != hash {
+        return Err(String::from("sha1 mismatch").into());
+    } // todo: add retry now its just an err
+
+    create_dir_all(&path_to_versions_dir)?;
+    write(&path_to_version_json, &res)?;
+    let json = from_slice::<ClientJson>(&res)?;
+
+    Ok(json)
 }
