869f9c99492fbcfd6ff42ac8203de6cfd332aaea
null68 <fariscomaga@gmail.com>
2026-08-11 23:40:40 +0200

    chore: initialize_launcher added to .setup()

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -12,7 +12,13 @@ async fn initialize_launcher() -> Result<(), String> {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![initialize_launcher, get_versions])
+        .setup(|_app| {
+            tauri::async_runtime::spawn(async {
+                initialize_launcher().await.unwrap();
+            });
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![get_versions])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
