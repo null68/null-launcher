853ae4c77029798a32eb302c516f9e80fc080490
null68 <fariscomaga@gmail.com>
2026-09-05 22:40:07 +0200

    fix(linux): disable problematic WebKit rendering modes
    Closes #1

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,17 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    #[cfg(target_os = "linux")]
+    {
+        for (key, val) in [
+            ("WEBKIT_DISABLE_DMABUF_RENDERER", "1"),
+            ("WEBKIT_DISABLE_COMPOSITING_MODE", "1"),
+        ] {
+            if std::env::var_os(key).is_none() {
+                unsafe { std::env::set_var(key, val) };
+            }
+        }
+    }
+
     null_launcher_lib::run()
 }
